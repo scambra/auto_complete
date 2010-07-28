79ae852aebadee36261a066e8ed276b70cde51ee
scambra <sergio@entrecables.com>
2010-07-28 13:12:04 +0200

    Fix div id generation, wasn't synchronized with generation way in auto_complete_field method

--- lib/auto_complete_macros_helper.rb
+++ lib/auto_complete_macros_helper.rb
@@ -112,10 +112,12 @@ module AutoCompleteMacrosHelper
   # auto_complete_for to respond the AJAX calls,
   # 
   def text_field_with_auto_complete(object, method, tag_options = {}, completion_options = {})
+    field_id = tag_options[:id] || "#{object}_#{method}"
+    div_id = completion_options[:update] || "#{field_id}_auto_complete"
     (completion_options[:skip_style] ? "" : auto_complete_stylesheet) +
     text_field(object, method, tag_options) +
-    content_tag("div", "", :id => "#{object}_#{method}_auto_complete", :class => "auto_complete") +
-    auto_complete_field("#{object}_#{method}", { :url => { :action => "auto_complete_for_#{object}_#{method}" } }.update(completion_options))
+    content_tag("div", "", :id => div_id, :class => "auto_complete") +
+    auto_complete_field(field_id, { :url => { :action => "auto_complete_for_#{object}_#{method}" } }.update(completion_options))
   end
 
   private
